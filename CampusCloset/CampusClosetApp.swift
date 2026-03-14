4b44fbc5a523e4b0549b2fb160b725107189014c
Jun Kuang <junkuang277@gmail.com>
2026-03-14 17:05:05 -0400

    Initial Commit

---
+++ CampusCloset/CampusClosetApp.swift
@@ -0,0 +1,17 @@
+//
+//  CampusClosetApp.swift
+//  CampusCloset
+//
+//  Created by Jun Kuang on 3/14/26.
+//
+
+import SwiftUI
+
+@main
+struct CampusClosetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CampusClosetTests/CampusClosetTests.swift
@@ -0,0 +1,17 @@
+//
+//  CampusClosetTests.swift
+//  CampusClosetTests
+//
+//  Created by Jun Kuang on 3/14/26.
+//
+
+import Testing
+@testable import CampusCloset
+
+struct CampusClosetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
