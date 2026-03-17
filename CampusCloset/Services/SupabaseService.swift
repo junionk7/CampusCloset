87f9dc9fd4fc65ebbd42711c496307f319d8fa20
Jun Kuang <junkuang277@gmail.com>
2026-03-16 22:24:15 -0400

    Start adding Supabase

---
+++ CampusCloset/Services/SupabaseService.swift
@@ -0,0 +1,14 @@
+//
+//  SupabaseService.swift
+//  CampusCloset
+//
+//  Created by Jun Kuang on 3/16/26.
+//
+
+import Supabase
+import Foundation
+
+let supabase = SupabaseClient(
+    supabaseURL: URL(string: "https://okfhhmlggallmzugfbgc.supabase.co")!,
+    supabaseKey: "sb_publishable_8EAmRPWMUI5sbVelmMT5tw_1esWE1fh"
+)
